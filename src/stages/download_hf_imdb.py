adc0564cf307bd40770ac0a0e1363c3e2c68b6c1
Daniel <archibasov.21.daniil.w@gmail.com>
2026-01-26 01:42:00 +0300

    Починил путь к data в ./stages/download_hf_imdb.py

--- src/stages/download_hf_imdb.py
+++ src/stages/download_hf_imdb.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from box import ConfigBox
 from datasets import load_dataset
-
+import datasets
 
 from src.utils.folder_management import create_folders
 from src.utils.decorator import parser
@@ -10,7 +10,8 @@ from src.utils.decorator import parser
 @parser(prog_name="Transform Pipeline", dscr="Split Naturally the data | Patchify it")
 def main(params: ConfigBox) -> None:
     data = load_dataset(params.hf_name, split='train')
-    data.save_to_disk(params.data_dir)
+    create_folders(params.download_hf_imdb.data_dir)
+    data.save_to_disk(Path(params.download_hf_imdb.data_dir) / params.download_hf_imdb.hf_name)
     
 
 if __name__ == "__main__":
