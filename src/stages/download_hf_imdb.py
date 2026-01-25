d832caede0ac226b122e82120b8d92128e09f11a
Daniil <archibasov.21.daniil.w@gmail.com>
2026-01-25 23:45:45 +0000

    fixed download and save dataset step

--- src/stages/download_hf_imdb.py
+++ src/stages/download_hf_imdb.py
@@ -9,8 +9,8 @@ from src.utils.decorator import parser
 
 @parser(prog_name="Transform Pipeline", dscr="Split Naturally the data | Patchify it")
 def main(params: ConfigBox) -> None:
-    data = load_dataset(params.hf_name, split='train')
-    create_folders(params.download_hf_imdb.data_dir)
+    data = load_dataset(params.download_hf_imdb.hf_name, split='train')
+    create_folders([Path(params.download_hf_imdb.data_dir)])
     data.save_to_disk(Path(params.download_hf_imdb.data_dir) / params.download_hf_imdb.hf_name)
     
 
